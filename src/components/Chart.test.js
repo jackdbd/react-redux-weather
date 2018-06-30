62247de606073ebb8e6b0dce82254a91c948c4b0
jackdbd <jackdebidda@gmail.com>
2018-06-30 15:04:18 +0200

    Update deploy script

--- src/components/Chart.test.js
+++ src/components/Chart.test.js
@@ -2,11 +2,13 @@ import React from "react";
 import { shallow, mount, render } from "enzyme";
 import "../setupEnzymeTests";
 import Chart from "./Chart";
-import {Sparkline} from "@data-ui/sparkline";
+import { Sparkline } from "@data-ui/sparkline";
 
 describe.skip("<Chart />", () => {
   it("renders one (and only one) <Sparkline />", () => {
-    const wrapper = shallow(<Chart title={"some title"} data={[]} tooltipId="someId" />);
+    const wrapper = shallow(
+      <Chart title={"some title"} data={[]} tooltipId="someId" />
+    );
     expect(wrapper.find(Sparkline)).toHaveLength(1);
   });
 });
